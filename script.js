ae1c72172d2082de0328b4908906d51b4d1dd005
Rafael Correa <rafaelscorreadev@gmail.com>
2024-02-26 19:23:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const prompt = require("prompt-sync") ({sigint: true})
+const numeros = []
+
+for(let i=0; i<5;i++){
+    numeros.push(parseInt(prompt(`Digite o ${i + 1} numero: `)))
+}
+
+for(let i=0; i<5;i++){
+    console.log(`O numero é ${numeros[i]}: `)
+}
+
+const soma = numeros.reduce( (acumulador, numeroAtual) =>{
+    return acumulador + numeroAtual
+}, 0)
+
+console.log("A soma dos números é: ", soma)
+
+const numerosOrdenados = [...numeros]
+numerosOrdenados.sort((num1,num2) => num1 - num2.join(" "))
+console.log(`A lista de numeros ordenados é : ${numerosOrdenados.join(" ")}`)
